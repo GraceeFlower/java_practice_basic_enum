551528f39bae82d95d4dc1c049d4d776bae5956d
GraceeFlower <1491644103@qq.com>
2020-02-13 14:10:28 +0800

    [WM] feat: add the function to filter the female student

--- enum/src/main/java/Application.java
+++ enum/src/main/java/Application.java
@@ -1,7 +1,14 @@
 public class Application {
 
   public static void main(String[] args) {
-
+      filterFemale();
   }
 
+  public static void filterFemale() {
+      for (Student i: Student.values()) {
+          if (i.filterSex(i.getSex())) {
+              System.out.println(i.getName());
+          }
+      }
+  }
 }
